db5df1520cd2ec287887d5c8ae19c4a7dbe2cad1
Anderson Martins <anderson.mazah@outlook.com>
2022-01-16 17:46:54 -0300

    add file

---
+++ Desafio_Xenlongao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Desafio_Xenlongao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // quantidade de casos de testes
+            int C = Convert.ToInt32(Console.ReadLine());
+            while(C-- > 0)
+            {
+                // pega a quantidade de esferas a serem testadas
+                int val = Convert.ToInt32(Console.ReadLine());
+                int newval = (int)Math.Sqrt(val);
+                int resposta = val - newval;
+                Console.WriteLine(resposta);                
+            }
+        }
+    }
+}
