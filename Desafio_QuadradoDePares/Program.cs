a467d807c420705364be5ed59d32071b77ba9ec9
Anderson Martins <anderson.mazah@outlook.com>
2022-01-16 18:29:30 -0300

    add file

--- Desafio_QuadradoDePares/Program.cs
+++ Desafio_QuadradoDePares/Program.cs
@@ -6,7 +6,13 @@ namespace Desafio_QuadradoDePares
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++){
+                if(i %2 == 0){
+                    int b = i * i;
+                    Console.WriteLine($"{i}^2 = {b}");
+                }
+            }
         }
     }
 }
