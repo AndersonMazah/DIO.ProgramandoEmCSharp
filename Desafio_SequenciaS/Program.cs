ed9356a7192b3720c2b1fc32cca8f772480d72fe
Anderson Martins <anderson.mazah@outlook.com>
2022-01-16 19:24:51 -0300

    add file

---
+++ Desafio_SequenciaS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Desafio_SequenciaS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, S = 1;
+            for (int i = 2; i <= 100; i ++)
+            {
+                c = 1.0 / i;
+                S += c;
+            }
+            var x = Math.Round(S, 2);
+            Console.WriteLine(x);
+         }
+    }
+}
