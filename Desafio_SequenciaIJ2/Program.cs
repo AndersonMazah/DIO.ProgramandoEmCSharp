dcf018774e1a86656fc6d50b5ca38725ee538b9c
Anderson Martins <anderson.mazah@outlook.com>
2022-01-16 19:45:46 -0300

    add file

---
+++ Desafio_SequenciaIJ2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Desafio_SequenciaIJ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            for(int i = 1; i <= 9; i = i + 2)
+            {
+                j = 7;
+                for(int ii = 1; ii <= 3; ii++)
+                {
+                    Console.WriteLine($"I={i} J={j--}");
+                }
+            }
+        }
+    }
+}
