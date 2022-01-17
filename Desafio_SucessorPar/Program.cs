87b7841b27b7304f3149a48b4abf1ac05475a140
Anderson Martins <anderson.mazah@outlook.com>
2022-01-16 21:18:35 -0300

    add file

---
+++ Desafio_SucessorPar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Desafio_SucessorPar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opc = Console.ReadLine();
+            if(int.TryParse(opc, out int x))
+            {
+                while(true) {
+                    x++;
+                    if(x % 2 == 0)
+                    {
+                        Console.WriteLine(x);
+                        break;
+                    }
+                }
+            }
+            
+        }
+    }
+}
