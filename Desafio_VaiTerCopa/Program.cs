300a2219a02faabc00a08dfb4c7b05e0e289badf
Anderson Martins <anderson.mazah@outlook.com>
2022-01-16 21:00:03 -0300

    add file

---
+++ Desafio_VaiTerCopa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Desafio_VaiTerCopa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opc;
+            while((opc = Console.ReadLine()) != null)
+            {
+                int x;
+                if(int.TryParse(opc, out x))
+                {
+                    if(x == 0) {
+                        Console.WriteLine("vai ter copa!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("vai ter duas!");
+                    }
+                }
+            }
+        }
+    }
+}
