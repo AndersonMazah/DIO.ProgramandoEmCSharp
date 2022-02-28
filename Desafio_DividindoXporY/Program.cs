f0eb4c6eaa7c1310ded5cc65540ca3e055223ffd
Anderson Martins <anderson.mazah@outlook.com>
2022-02-28 00:59:24 -0300

    add file

---
+++ Desafio_DividindoXporY/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Desafio_DividindoXporY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Você terá o desafio de escrever um algoritmo que leia 2 números e imprima o resultado da divisão do primeiro pelo segundo.
+            Caso não for possível, mostre a mensagem “divisao impossivel” para os valores em questão.
+
+            Entrada
+            A entrada contém um número inteiro N. Este N será a quantidade de pares de valores inteiros (X e Y) que serão lidos em seguida.
+
+            Saída
+            Para cada caso mostre o resultado da divisão com um dígito após o ponto decimal, ou “divisao impossivel” caso não seja possível efetuar o cálculo.
+            */
+            int limit = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < limit; i++) 
+            {
+                string[] line = Console.ReadLine().Split(" ");
+                double X = double.Parse(line[0]);
+                double Y = double.Parse(line[1]);
+                if (Y == 0) {
+                    Console.WriteLine("divisao impossivel");
+                } else {
+                    double divisao = X / Y;
+                    Console.WriteLine(divisao.ToString("0.0"));
+                }
+            }
+        }
+    }
+}
