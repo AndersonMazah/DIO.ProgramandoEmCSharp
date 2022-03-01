c3f99216c8b1c1579c49efa02234660e6bb0fa25
Anderson Martins <anderson.mazah@outlook.com>
2022-02-28 21:33:39 -0300

    add file

---
+++ Desafio_FormulaDeBhaskara/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Desafio_FormulaDeBhaskara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara.
+            Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
+
+            Entrada
+                Leia três valores de ponto flutuante (double) A, B e C.
+
+            Saída
+                Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular".
+                Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo.
+                Imprima sempre o final de linha após cada mensagem.
+            */
+            double a, b, c, delta, r1, r2;
+            string[] valor = Console.ReadLine().Split();
+
+            a = Convert.ToDouble(valor[0]);
+            b = Convert.ToDouble(valor[1]);
+            c = Convert.ToDouble(valor[2]);
+
+            delta = (Math.Pow(b, 2) - (4 * a * c));
+            r1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            r2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+            if (delta >= 0 && a != 0)
+            {
+                Console.WriteLine($"R1 = {String.Format("{0:0.00000}", r1)}\nR2 = {String.Format("{0:0.00000}", r2)}");
+            }
+            else
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+        }
+    }
+}
