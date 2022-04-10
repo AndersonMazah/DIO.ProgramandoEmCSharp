50fffe7d3cf3cc041375439dbc045f80864c414e
Anderson Martins <anderson.mazah@outlook.com>
2022-04-10 13:00:09 -0300

    add file

---
+++ Desafio_Diferenca/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            Leia quatro valores inteiros A, B, C e D.
+            A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D)
+            */
+            int A = 0;
+            int B = 0;
+            int C = 0;
+            int D = 0;
+            if (
+            int.TryParse(Console.ReadLine(), out A) &&
+            int.TryParse(Console.ReadLine(), out B) &&
+            int.TryParse(Console.ReadLine(), out C) &&
+            int.TryParse(Console.ReadLine(), out D))
+            {
+                Console.WriteLine($"DIFERENCA = {(A * B) - (C * D)}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Desafio_Multiplos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            Leia 2 valores inteiros (A e B).
+            Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+            */
+            int A = 0;
+            int B = 0;
+            if (int.TryParse(Console.ReadLine(), out A) && int.TryParse(Console.ReadLine(), out B))
+            {
+                if (A % B == 0 || B % A == 0)
+                {
+                    Console.WriteLine($"Sao Multiplos");
+                }
+                else
+                {
+                    Console.WriteLine($"Nao sao Multiplos");
+                }
+            }
+        }
+    }
+}
